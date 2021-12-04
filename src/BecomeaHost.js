7c4ad1d9136c291676b580f4c6fc282bb969daea
nickimariecarr <80299874+nickimariecarr@users.noreply.github.com>
2021-12-03 22:38:56 -0500

    form changes

--- src/BecomeaHost.js
+++ src/BecomeaHost.js
@@ -1,142 +1,175 @@
-import emailjs from 'emailjs-com'
-import {Button} from 'react-bootstrap'
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import emailjs from 'emailjs-com';
+import { Form, Input, TextArea, Button } from "semantic-ui-react";
+import Swal from "sweetalert2";
 
+const SERVICE_ID = "teenytinyexplorers";
+const TEMPLATE_ID = "template_kxw5tph";
+const USER_ID = "user_a5jwzuX6kSAOK7KdHVqVM";
 
 const BecomeaHost = () => {
-    
-    
-    function sendEmail(event){
-        event.preventDefault();
-
-        emailjs.sendForm(
-        "teenytinyexplorers",
-        "template_kxw5tph", 
-        event.target, 
-        "user_a5jwzuX6kSAOK7KdHVqVM"
-    ).then(res=>{ 
-        navigate("/home");
-        console.log(res);
-    }).catch(err=>console.log(err));
-
-    } 
-
-    let navigate = useNavigate()
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-        sendEmail(e.target);
-        navigate("./home", { replace: true });
-    };
-    
   
-  
-    return (
-        <div>
+  {const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm( SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then((result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Thank you for reaching out to us! Please allow us 2-3 business days to respond to your initial email."
+        })
+      }, (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong. Please try again, if the problem continues please email us at teenytinyexplorers@gmail.com",
+          text: error.text,
+        })
+      });
+    e.target.reset()
+  };
 
-        
-        <div className="container border" style={{marginTop:"50px",width:"80%", background:"#6D7A69"}}>
-            <center><h1 style={{marginTop: "25px"}} > Contact Form </h1></center>
+  return (
+    <div className="ContactApp">
+    <h1> Become a Host </h1>
+    <h3> Want to become a Teeny Tiny Explorers host? </h3>
 
-            <form onClick={e => { this.sendEmail(e); this.handleSubmit() }} className="row" style={{margin: "25px 85px 75px 100px"}}  >
-                    <label>First Name</label>
-                    <input type="text" name="firstname"  className="form-control" />
-
-                    <label>Last Name</label>
-                    <input type="text" name="lastname" className="form-control" />
-                
-                    <label>Email</label>
-                    <input type="email" name="email" className="form-control"/>
-
-                    <label>Phone Number</label>
-                    <input type="phone" name="phonenumber" className="form-control"/>
-
-                    <label>Address</label>
-                    <input type="text" name="address1" className="form-control"/>
-
-                    <label>Address 2</label>
-                    <input type="text" name="address2" className="form-control" />
-
-
-                <center>
-                    <label >City.</label>
-                    <input type="text" name="city" className="form-control"/>
-
-                    <label>County</label>
-                    <input type="text" name="county" className="form-control" />
-
-                    <label>State</label>
-                    <select type="select"  name="state" className="form-control" defaultValue="Choose...">
-                      <option>Choose...</option>
-                      <option>AL</option>
-                      <option>AK</option>
-                      <option>AZ</option>
-                      <option>AR</option>
-                      <option>CA</option>
-                      <option>CO</option>
-                      <option>CT</option>
-                      <option>FL</option>
-                      <option>GA</option>
-                      <option>HI</option>
-                      <option>ID</option>
-                      <option>IL</option>
-                      <option>IN</option>
-                      <option>IA</option>
-                      <option>KS</option>
-                      <option>KY</option>
-                      <option>LA</option>
-                      <option>ME</option>
-                      <option>MD</option>
-                      <option>MA</option>
-                      <option>MI</option>
-                      <option>MN</option>
-                      <option>MS</option>
-                      <option>MO</option>
-                      <option>MT</option>
-                      <option>NE</option>
-                      <option>NV</option>
-                      <option>NH</option>
-                      <option>NJ</option>
-                      <option>NM</option>
-                      <option>NY</option>
-                      <option>NC</option>
-                      <option>ND</option>
-                      <option>OH</option>
-                      <option>OK</option>
-                      <option>OR</option>
-                      <option>PA</option>
-                      <option>RI</option>
-                      <option>SC</option>
-                      <option>SD</option>
-                      <option>TN</option>
-                      <option>TX</option>
-                      <option>UT</option>
-                      <option>VT</option>
-                      <option>VA</option>
-                      <option>WA</option>
-                      <option>WV</option>
-                      <option>WI</option>
-                      <option>WY</option>
-                    </select>
-
-                    <label>Zip</label>
-                    <input type="text" name="zip" className="form-control"/>
-                </center>
+        <p>
+            Teeny Tiny Explorers continues to grow as more adults are wanting to find ways to get their children outside and in the loving arms of nature. 
+            Teeny Tiny Explorers is always accepting applications for hosts. The more that volunteer their time, the more little lives we can all impact. 
+        </p>
+    <h3>What is a group host?</h3>
+        <p>
+            A group host is a volunteer that lives in a county they would like to host a group in. 
+            The group host is in charge of running meetings, and events in their area and moderating conversations 
+            amount members
+        </p>
+    <h3>What does a group host do?</h3>
+        <p>
+            A group host is in charge of keeping conversations together, and making sure all members are treated with respect. Group hosts are 
+            leaders that are encouraged to create environments where engaging conversations and activities can take place for children and adults alike. Group hosts will always be able to talk to one another and get extra support where needed. 
+        </p>
 
 
-              <center> Please indicate your preferred contact method below message </center>
-                <label controlId="floatingTextarea2" >
-                  <input type="text" name="preferred" as="textarea" placeholder="Leave a comment here"
-                    style={{ height: '100px', width: "100%" }} className="form-control"/>
-                </label>
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Field
+          id="form-input-control-email"
+          control={Input}
+          label="Email"
+          name="email"
+          placeholder="Email..."
+          required
+          icon="mail"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-input-control-first-name"
+          control={Input}
+          label="First Name"
+          name="firstname"
+          placeholder="FirstName…"
+          required
+          icon="user circle"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-input-control-last-name"
+          control={Input}
+          label="Last Name"
+          name="lastname"
+          placeholder="Last Name…"
+          required
+          icon="user circle"
+          iconPosition="left"
+        />
+
+        <Form.Field
+          id="form-textarea-control-Phone"
+          control={Input}
+          label="Phone"
+          name="phone"
+          placeholder="Phone…"
+          required
+          icon= "phone"
+          iconPosition="left"
+        />
+
+        <Form.Field
+          id="form-textarea-control-Address"
+          control={Input}
+          label="Address"
+          name="address1"
+          placeholder="Address…"
+          required
+          icon= "home"
+          iconPosition="left"
+        />
 
 
-                <Button type="Submit" style={{marginTop: "30px",background:"white", color:"black"}}>Submit</Button>
-            
-            </form>
-        </div>
+        <Form.Field
+          id="form-textarea-control-Address"
+          control={Input}
+          label="Address 2"
+          name="address2"
+          placeholder="Address 2…"
+          icon= "home"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-textarea-control-City"
+          control={Input}
+          label="City"
+          name="city"
+          placeholder="City…"
+          icon= "home"
+          iconPosition="left"
+        />
+
+        <Form.Field
+          id="form-textarea-control-state"
+          control={Input}
+          label="State"
+          name="state"
+          icon= "home"
+          iconPosition="left"
+          placeholder="State…"
+          required
+        />
+
+        <Form.Field
+          id="form-textarea-control-county"
+          control={Input}
+          label="County"
+          name="county"
+          icon= "home"
+          iconPosition="left"
+          placeholder="County…"
+          required
+        />
+
+        <Form.Field
+          id="form-textarea-control-zip"
+          control={Input}
+          label="Zip"
+          name="zip"
+          icon= "home"
+          iconPosition="left"
+          placeholder="Zip…"
+          required
+        />
+
+        <Form.Field
+          id="form-textarea-control-opinion"
+          control={TextArea}
+          label="Comments"
+          name="message"
+          placeholder="Please provide information on why you would like to become a host, here…"
+          required
+        />
+        <Button type="submit" color="lightmaincolor">Submit</Button>
+      </Form>
     </div>
-    );
-};
-
+  );
+}};
 export default BecomeaHost;
--- src/contact.js
+++ src/contact.js
@@ -1,46 +1,103 @@
+import React from "react";
+import "./App.css";
 import emailjs from 'emailjs-com';
-import { Link } from "react-router-dom"; 
+import { Form, Input, TextArea, Button } from "semantic-ui-react";
+import Swal from "sweetalert2";
 
-//npm install emailjs-com --save
+const SERVICE_ID = "teenytinyexplorers";
+const TEMPLATE_ID = "testcontact";
+const USER_ID = "user_a5jwzuX6kSAOK7KdHVqVM";
+
 const Contact = () => {
-    function sendEmail(e){
-        e.preventDefault();
+  
+  {const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm( SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then((result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Thank you for reaching out to us! Please allow us 2-3 business days to respond to your initial email."
+        })
+      }, (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong. Please try again, if the problem continues please email us at teenytinyexplorers@gmail.com",
+          text: error.text,
+        })
+      });
+    e.target.reset()
+  };
 
-        emailjs.sendForm(
-        "teenytinyexplorers",
-        "testcontact", 
-        e.target, 
-        "user_a5jwzuX6kSAOK7KdHVqVM"
-    ).then(res=>{
-        console.log(res);
-    }).catch(err=>console.log(err));
+  return (
+    <div className="ContactApp">
+    <h1>Contact Us</h1>
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Field
+          id="form-input-control-email"
+          control={Input}
+          label="Email"
+          name="email"
+          placeholder="Email..."
+          required
+          icon="mail"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-input-control-first-name"
+          control={Input}
+          label="First Name"
+          name="firstname"
+          placeholder="FirstName…"
+          required
+          icon="user circle"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-input-control-last-name"
+          control={Input}
+          label="Last Name"
+          name="lastname"
+          placeholder="Last Name…"
+          required
+          icon="user circle"
+          iconPosition="left"
+        />
 
-    }
-    return (
-        <div className="container border" style={{marginTop:"50px",width:"50%", background:"#6D7A69"}}>
-            <center><h1 style={{marginTop: "25px"}} > Contact Form </h1></center>
-            <form className="row" style={{margin: "25px 85px 75px 100px"}} onSubmit={sendEmail}>
-                <label>First Name</label>
-                <input type ="text" name="first_name" className="form-control"/>
+        <Form.Field
+          id="form-textarea-control-Phone"
+          control={Input}
+          label="Phone"
+          name="phone"
+          placeholder="Phone…"
+          required
+          icon= "phone"
+          iconPosition="left"
+        />
 
-                <label>Last Name</label>
-                <input type ="text" name="last_name" className="form-control"/>
+        <Form.Field
+          id="form-textarea-control-opinion"
+          control={Input}
+          label="Preferred Contact Time"
+          name="message"
+          icon= "clock"
+            iconPosition="left"
+          placeholder="Please let us know if you rather be contact in the morning, afternoon, or evening…"
+          required
+        />
 
-                <label> Email </label>
-                <input type="email" name="user_email" className="form-control"/>
-                
-                <label> Phone Number </label>
-                <input type="phone" name="phone" className="form-control"/>
-
-                <label> Best Time To Contact You</label>
-                <input type="text" name="time" className="form-control"/>
-
-                <label> Message </label>
-                <textarea name="message" rows="4" className="form-control"/>
-                <input type="submit" value="Send" className="form-control" style={{marginTop: "30px",background:"white", color:"black"}} />
-            </form>
-        </div>
-    );
-};
-
+        <Form.Field
+          id="form-textarea-control-opinion"
+          control={TextArea}
+          label="Comments"
+          name="message"
+          placeholder="Please provide questions and concerns you would like to send to us, here…"
+          required
+        />
+        <Button type="submit" color="lightmaincolor">Submit</Button>
+      </Form>
+    </div>
+  );
+}};
 export default Contact;
