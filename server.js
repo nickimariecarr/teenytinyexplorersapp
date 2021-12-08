c8487ab9c4ec84c2ad8f43fa3b2fd4f23f1080ea
nickimariecarr <80299874+nickimariecarr@users.noreply.github.com>
2021-12-08 07:43:42 -0500

    commit for port change on server.js

--- server.js
+++ server.js
@@ -20,9 +20,10 @@ const db = mysql.createPool({
 
   
 
-  const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log('App is listening on port ' + listener.address().port)
-})
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
 
 
 //********************************GROUPS TABLE*******************************************
