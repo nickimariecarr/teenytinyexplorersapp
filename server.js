bdc5bf338439b5573c43bbe57bc2144d3f2e3b23
nickimariecarr <80299874+nickimariecarr@users.noreply.github.com>
2021-12-10 13:06:20 -0500

    commit for deploy to heroku 3

--- server.js
+++ server.js
@@ -33,6 +33,18 @@ app.listen(PORT, () => {
     app.use(proxy(['/group' ], { target: 'http://localhost:3000' }));
 } 
 
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //********************************GROUPS TABLE*******************************************
 
 //get groups
